458185d225bc70ed0c565debbc22a14b4b5e1a19
louay <ayyl0uu@pm.me>
2021-07-25 17:32:53 +0100

    changed commands help from config.json to help.py

---
+++ help.py
@@ -0,0 +1,5 @@
+HELP = {
+    "read": "Sends manga chapter from readm.org by the manga number and chapter number.\nEX: read 822 1",
+    "manga": "Sends information about a manga by it's name",
+    "search": "Search for manga number by name"
+}
